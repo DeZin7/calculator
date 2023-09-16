1a4bd398270d032a4a21c223025dcac9c02e99c5
DeZin7 <marcusandre77@icloud.com>
2023-09-16 10:13:04 -0700

    Changes in unit test.

--- src/test/java/com/dezin/calculator/CalculatorTest.java
+++ src/test/java/com/dezin/calculator/CalculatorTest.java
@@ -4,7 +4,7 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 public class CalculatorTest {
-    
+    private Calculator calculator = new Calculator();
     
     @Test 
     public void testSum() {
